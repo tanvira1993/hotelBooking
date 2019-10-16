4d3db832b54719b9ae18c68ea223c4631c8d21b1
tanvira <tanvira1993@gmail.com>
2019-10-17 03:35:34 +0600

    Theme implemented fully

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,40 +1,24 @@
+ <div style="margin-top: 3%">
+ 	<div id="wrapper">
+ 		<form > 
+ 			<h1>Log in</h1> 
+ 			<p> 
+ 				<label for="username" class="uname" >  Email</label>
+ 				<input id="username" name="username" required="required" type="text" placeholder="myusername or mymail@mail.com"/>
+ 			</p>
+ 			<p> 
+ 				<label for="password" class="youpasswd" > Password </label>
+ 				<input id="password" name="password" required="required" type="password" placeholder="eg. X8df!90EO" /> 
+ 			</p>
+ 			<p class="login button"> 
+ 				<input type="submit" value="Login" /> 
+ 			</p>
+ 			<p>
+ 				Not a member yet ?
+ 				<a href="#toregister" class="to_register">Register</a>
+ 			</p>
 
+ 		</form>
+ 	</div>
 
-       
-        <link rel="stylesheet" type="text/css" href="frontend/styleLogin.css" />
-    
-   
-        <div class="container" style="margin-top: 3%">
-            <section>				
-                <div id="container_demo" >
-                    <a class="hiddenanchor" id="toregister"></a>
-                    <a class="hiddenanchor" id="tologin"></a>
-                    <div id="wrapper">
-                        <div id="login" class="animate form">
-                            <form  action="mysuperscript.php" autocomplete="on"> 
-                                <h1>Log in</h1> 
-                                <p> 
-                                    <label for="username" class="uname" > Your email or username </label>
-                                    <input id="username" name="username" required="required" type="text" placeholder="myusername or mymail@mail.com"/>
-                                </p>
-                                <p> 
-                                    <label for="password" class="youpasswd" > Your password </label>
-                                    <input id="password" name="password" required="required" type="password" placeholder="eg. X8df!90EO" /> 
-                                </p>
-                                
-                                <p class="login button"> 
-                                    <input type="submit" value="Login" /> 
-								</p>
-                                <p class="change_link">
-									Not a member yet ?
-									<a href="#toregister" class="to_register">Join us</a>
-								</p>
-                            </form>
-                        </div>
-
-                        
-                    </div>
-                </div>  
-            </section>
-        </div>
-   
+ </div>
